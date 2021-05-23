89db3e5737176f8c141888d3b90264e1ceb1ae49
123arnav <74090758+123arnav@users.noreply.github.com>
2021-05-23 18:23:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function preload(){
+SongA="";
+SongB="";
 
+function preload(){
+    SongA=loadSound("one_song.mp3")
+    SongB=loadSound("one_song.mp3")
 }
 
 function setup(){
-    canvas=createCanvas(300,300);
+    canvas=createCanvas(400,350);
     canvas.position(430,230);
-
+video=createCapture(VIDEO)
+video.hide();
 };
 function draw(){
-
+image(video,0,0,400,350)
 };
