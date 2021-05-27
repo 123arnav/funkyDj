4f5969f4f21adb65781cb01cbdd1a43337ce35ed
123arnav <74090758+123arnav@users.noreply.github.com>
2021-05-27 11:08:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 SongA="";
 SongB="";
+LeftWristY="";
+LeftWristX="";
+RightWristY="";
+RightWristX="";
 
 function preload(){
     SongA=loadSound("one_song.mp3")
@@ -11,7 +15,28 @@ function setup(){
     canvas.position(430,230);
 video=createCapture(VIDEO)
 video.hide();
+posenet=ml5.poseNet(video,modelLoaded);
+console.log(ml5.version);
+posenet.on("pose", getposes);
 };
+function modelLoaded(){
+    console.log("loaded")
+    };
+
+    function getposes(result){
+        if(result.length>0){
+            console.log(result);
+            leftWristY=result[0].pose.leftWrist.y;
+            leftWristX=result[0].pose.leftWrist.x;
+            console.log(leftWristY);
+            console.log(leftWristX);
+            rightWristY=result[0].pose.rightWrist.y;
+            rightWristX=result[0].pose.rightWrist.x;
+            console.log(rightWristY);
+            console.log(rightWristX);
+        }
+    }
+
 function draw(){
 image(video,0,0,400,350)
 };
